555fca5fbde8b54b17e9280631b4821482217085
Nisarg Vadgama <nisargvv@gmail.com>
2022-12-05 09:29:25 -0800

    Update

--- src/action.js
+++ src/action.js
@@ -1,10 +1,10 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+import { getInput } from '@actions/core';
+import github, { getOctokit } from '@actions/github';
 
 async function run() {
-  const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+  const GITHUB_TOKEN = getInput('GITHUB_TOKEN');
 
-  const octokit = github.getOctokit(GITHUB_TOKEN);
+  const octokit = getOctokit(GITHUB_TOKEN);
 
   const { context = {} } = github;
   const { pull_request } = context.payload;
