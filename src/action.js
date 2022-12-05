54519771f176cf7638a3b216a5830a651e4cadd0
Nisarg Vadgama <nisargvv@gmail.com>
2022-12-05 12:00:25 -0800

    Update

--- src/action.js
+++ src/action.js
@@ -6,23 +6,24 @@ async function run() {
   const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
   const TENOR_TOKEN = core.getInput('TENOR_TOKEN');
 
+  const randomPos = Math.round(Math.random() * 1000);
+  const url = `https://tenor.googleapis.com/v2/search?q=thank%20you&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
+
+  const response = await fetch(url);
+  const { results } = await response.json();
+  const gifUrl = results[0].media[0].tinygif.url;
+
   const octokit = github.getOctokit(GITHUB_TOKEN);
 
   const { context = {} } = github;
   const { pull_request } = context.payload;
-
-  const randomPos = Math.round(Math.random() * 1000);
-  const url = `https://api.tenor.com/v1/search?q=thank%20you&pos=${randomPos}&limit=1&media_filter=minimal&contentfilter=high&key=${TENOR_TOKEN}`;
-  const response = await fetch(url);
-  const { results } = await response.json();
-  const gifUrl = results[0].media[0].tinygif.url;
 
   await octokit.rest.issues.createComment({
     ...context.repo,
     issue_number: pull_request.number,
     body: `Thank you for submitting a pull request! I will try to review this as soon as we can.\n\n<img src="${gifUrl}" alt="thank you" />`
   });
-  
+
 }
 
 run();
